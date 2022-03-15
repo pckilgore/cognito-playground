826ab5d81d81eed242b5df7c6ec8ba31cfcd9b76
Patrick Kilgore <git@pck.email>
2022-03-15 12:03:27 -0500

    feat: no rescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { Auth, Hub } from "aws-amplify";
-import { authExchange } from "@urql/exchange-auth";
-import { makeOperation, dedupExchange, fetchExchange } from "@urql/core";
-import { cacheExchange } from "@urql/exchange-graphcache";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { createClient, Provider } from "urql";
 import { Env, isCookie } from "./Environment";
-import { Main } from "./Main.gen";
 
 Auth.configure({
   region: Env.REACT_APP_AWS_REGION,
@@ -70,60 +65,9 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
   ) : null;
 }
 
-function GraphQLProvider({ children }: { children: React.ReactNode }) {
-  const { Auth } = useAuth();
-
-  const client = React.useMemo(
-    () =>
-      createClient({
-        url: Env.REACT_APP_ENDPOINT,
-        exchanges: [
-          dedupExchange,
-          cacheExchange({}),
-          authExchange({
-            addAuthToOperation: ({ authState, operation }) => {
-              if (!authState) {
-                return operation;
-              }
-              const token = authState.getIdToken().getJwtToken();
-              return makeOperation(operation.kind, operation, {
-                ...operation.context,
-                fetchOptions: {
-                  headers: {
-                    ...(token ? { Authorization: `Bearer ${token}` } : {}),
-                  },
-                },
-              });
-            },
-            willAuthError: ({ authState }) => {
-              if (!authState) return true;
-              return !authState.isValid();
-            },
-            didAuthError: ({ error }) => {
-              // check if the error was an auth error (this can be implemented in various ways, e.g. 401 or a special error code)
-              // TODO(pckilgore)
-              return error.graphQLErrors.some(
-                (e) => e.extensions?.code === "FORBIDDEN"
-              );
-            },
-            getAuth: () => {
-              return Auth.currentSession();
-            },
-          }),
-          fetchExchange,
-        ],
-      }),
-    [Auth]
-  );
-
-  return <Provider value={client}>{children}</Provider>;
-}
-
 export default withAuthenticator(() => (
   <AuthProvider>
-    <GraphQLProvider>
-      <Main />
-      <AmplifySignOut />
-    </GraphQLProvider>
+    <div>App Goes here</div>
+    <AmplifySignOut />
   </AuthProvider>
 ));
